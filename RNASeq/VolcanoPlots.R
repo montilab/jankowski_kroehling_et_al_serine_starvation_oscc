620a55e716c6f25c6b95ebdcdf8246d2826880b7
Lina Kroehling <72235521+lkroeh@users.noreply.github.com>
2024-10-25 12:02:52 -0400

    Update VolcanoPlots.R

--- RNASeq/VolcanoPlots.R
+++ RNASeq/VolcanoPlots.R
@@ -1,5 +1,8 @@
 library(EnhancedVolcano)
 
+#read in DESeq2 outputs
+hh <- read.csv('./results/DESeq2/20230501_H_DEout.csv')
+cc <- read.csv('./results/DESeq2/20230501_C_DEout.csv')
 
 
 keyvals <-  ifelse(
